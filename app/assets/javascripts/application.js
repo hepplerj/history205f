385ac524868a73057c5aeea362f46fe08bc7b7a4
paulc3 <paulc3@stanford.edu>
2014-11-10 16:12:10 -0800

    about to move stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,19 +40,73 @@
            		return;
        		}
        		if (this.status == 200) {
-              var topten = JSON.parse(this.responseText);
+            var topten = JSON.parse(this.responseText);
 
-              var table = "<table class=\"table\"><tr><th>Name</th><th>Articles</th></tr>";
-              for (var topic in topten) {
-                var split = topic.split("|");
-                table = table.concat("<tr><td>" + split[0] + "</td><td>" + split[1] + "</td></tr>");
-              }
+            var width = 100;
+            var height = 25;
+            var x = d3.scale.linear().range([0, width]);
+            var y = d3.scale.linear().range([height, 0]);
 
-           		table = table.concat("</table>");
-              $("#container").html(table);
-           	} else {
-           		console.log("Error.");
-              $("#container").html("<h3>Insufficient data. Please try again.</h3>");
+            console.log("PAST SCALE");
+
+            var parser = d3.time.format("%Y-%m-%d").parse;
+            start = parser(startYear+"-01-01");
+            end = parser(endYear+"-12-31");
+
+            console.log("PAST PARSE");
+
+            x.domain([start, end]);
+
+            console.log("PAST DOMAIN");
+
+            var line = d3.svg.line()
+             .x(function(d) { return x(d.date); })
+             .y(function(d) { return y(d.articles); });
+
+            var table = "<table class=\"table\"><tr><th>Name</th><th>Articles</th><th>Sparkline</th></tr>";
+            for (var topic in topten) {
+              var split = topic.split("|");
+              table = table.concat("<tr><td>" + split[0] + "</td><td>" + split[1] + "</td><td id=\"" + split[0] + "\"></td></tr>");
+            }
+
+         		table = table.concat("</table>");
+            $("#container").html(table);
+
+
+            console.log("BEGIN");
+
+
+
+            console.log("PAST LINE");
+
+            for (var topic in topten) {
+              var id = '#' + topic.split("|")[0];
+
+              console.log(id);
+
+              var data = topten[topic];
+
+              var parseDate = d3.time.format("%Y-%m-%d").parse;
+              data.forEach(function(datum) {
+                  datum.date = parseDate(datum.year);
+                  datum.articles = parseInt(datum.articles);
+              });
+              y.domain(d3.extent(data, function(d) { return d.articles; }));
+
+              console.log("PAST DOMAIN");
+
+              $(id).html = "BOO";
+                append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .append('path')
+                .datum(data)
+                .attr('class', 'sparkline')
+                .attr('d', line);
+            }
+          } else {
+           	console.log("Error.");
+            $("#container").html("<h3>Insufficient data. Please try again.</h3>");
        		}
        	}	
         myXHR.send();
